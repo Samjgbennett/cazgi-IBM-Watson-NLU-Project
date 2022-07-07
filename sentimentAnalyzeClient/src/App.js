2dc7764322be0eeff80d63b7a103a5f13d069a7e
Samjgbennett <108269274+Samjgbennett@users.noreply.github.com>
2022-07-07 16:29:49 +0100

    Update App.js

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -49,9 +49,8 @@ class App extends React.Component {
         let output = data.label;
         let color = "white"
         switch(output) {
-          case "positive": color = "green";break;
-          case "negative": color = "red";break;
-          case "neutral": color = "yellow";break;
+          case "positive": color = "black";break;
+          case "negative": color = "black";break;
           default: color = "black";
         }
         output = <div style={{color:color,fontSize:20}}>{output}</div>
